990c5a4dc475a22ec419a664a3e1138e016e61fe
Sam Turner <Sam.Turner@kyndryl.com>
2024-01-25 11:34:38 +1100

    make grid creation function and style grid

---
+++ script.js
@@ -0,0 +1,31 @@
+gridContainer = document.querySelector('#gridContainer');
+
+function gridSquareCreator(row) {
+    let gridSquare = document.createElement('div');
+    gridSquare.setAttribute('class','gridSquare');
+    row.appendChild(gridSquare);
+}
+
+function gridRowCreator(x){
+    for (let i = 0; i < x; i++){
+        let gridRow = document.createElement('div');
+        gridRow.setAttribute('class','gridRow');
+        gridContainer.appendChild(gridRow)
+    }
+}
+
+function gridCreator(x) {
+    gridRowCreator(x)
+    gridRows = document.querySelectorAll('.gridRow')
+    gridRows.forEach((row) => {
+        for (let i = 0; i < x; i++){
+            gridSquareCreator(row)
+        }
+    })
+}
+
+gridCreator(16)
+
+
+
+
